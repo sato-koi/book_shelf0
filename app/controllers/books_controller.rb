756f87c72468ceca5513d058277cbe8fe7f54e06
sato-koi <sato.koichi.0404@gmail.com>
2019-01-08 13:11:56 +0900

    画像登録機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :price, :publish_date, :description)
+    params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
   end
 
 end
